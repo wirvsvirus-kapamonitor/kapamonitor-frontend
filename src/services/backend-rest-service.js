0d5b795ca35663b963c13899e6d9b34940d9b744
Sebastian Boolean <truefalse@posteo.de>
2020-03-22 11:50:06 +0100

    implement rest-service

---
+++ src/services/backend-rest-service.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const BASE_URL = "http://wirvsvirusapplication-1852808866.eu-central-1.elb.amazonaws.com/api";
+
+const instance = axios.create({baseURL: BASE_URL, timeout: 8000, headers: {accept: "text/plain"}});
+
+export const getAllLocations = () => instance.get("/Location");
