7a2c520825871196170a32cdfcf86d7146e4d09d
Christoph Biernoth <christoph.biernoth@outlook.com>
2020-03-29 14:08:32 +0200

    Oidc userManager

--- src/containers/Signin-oidc.js
+++ src/containers/Signin-oidc.js
@@ -4,43 +4,53 @@ import Typography from '@material-ui/core/Typography';
 
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import Authenticate from 'react-openidconnect';
 import { swapThemeColors, toggleThemeMode } from '../store/settings/settings';
 import PaddingLayout from '../components/PaddingLayout';
 
+import Oidc from 'oidc-client';
+import axios from 'axios';
 
-var OidcSettings = {
 
-};
+var OidcSettings = {};
 
 class SigninOidc extends Component {
     constructor(props) {
         super(props);
-        this.userLoaded = this.userLoaded.bind(this);
-        this.userUnLoaded = this.userUnLoaded.bind(this);
 
-        this.state = { user: undefined };
+
     }
 
-    userLoaded(user) {
-        if (user)
-            this.setState({ "user": user });
+    componentDidMount() {
+
+        var userManager = new Oidc.UserManager(); 
+        userManager.signinCallback().then(res => {
+            console.log(res);
+        });
     }
 
-    userUnLoaded() {
-        this.setState({ "user": undefined });
+
+    onButtonClick = () => {
+        var config = {
+            authority: "https://kapamonitordev.b2clogin.com/kapamonitordev.onmicrosoft.com/B2C_1_susi",
+            client_id: "1a779c89-f148-4322-991b-bc94c99ddd88",
+            redirect_uri: "http://localhost:3000/signin-oidc",
+            post_logout_redirect_uri: 'http://localhost:3000/',
+            response_type: "id_token token",
+            response_type: "code",
+            //scope: "https://kapamonitordev.onmicrosoft.com/devapi/user_impersonation",
+            filterProtocolClaims: true,
+            loadUserInfo: true
+        };
+        
+        var userManager = new Oidc.UserManager(config);
+        userManager.signinRedirect();
     }
 
-    NotAuthenticated() {
-        return <div>You are not authenticated, please click here to authenticate.</div>;
-    }
     render() {
         return (
             <PaddingLayout>
-                <Typography variant="headline">Login</Typography>
-                <Authenticate OidcSettings={OidcSettings} userLoaded={this.userLoaded} userunLoaded={this.userUnLoaded} renderNotAuthenticated={this.NotAuthenticated}>
-                    <div>If you see this you are authenticated.</div>
-                </Authenticate>
+                <Typography variant="headline">SignIn</Typography>
+                <button onClick={this.onButtonClick}>Click!</button>
             </PaddingLayout>
         );
     }
